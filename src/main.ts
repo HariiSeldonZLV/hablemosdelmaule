c4078f7efbaf3543e866745e22543892a1d1ec4b
Denis Salinas <dje.salinas.m@gmail.com>
2026-06-16 15:51:40 -0400

    Resolver conflictos en main.ts e instalar Angular Material

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,6 @@
-<<<<<<< HEAD
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
-=======
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-
-// Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-// CSS global
-import "./style.css";
-
-createApp(App)
-  .use(router) 
-  .mount("#app");
->>>>>>> d22be05e0ffe24e3496e6d2d60d97363a5580749
